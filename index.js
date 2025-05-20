88d2411069faa51c8afb0addc095ad3088f9c3f8
smabari <mdazizulbarishafin@gmail.com>
2025-05-20 07:36:07 +0600

    server data setup

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const posts = require("./posts.json");
 const app = express();
 const port = 5000;
 
 app.get("/", (request, response) => {
-  response.send("Hello from my first server but not the last really");
+  response.send("My Express server");
 });
 
-app.get('/data',(request,response)=>{
-  response.send('More data is comming soon')
-})
+app.get("/posts", (request, response) => {
+  response.send(posts);
+});
+
+app.get("/posts/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log("i need data for id", id);
+  const post = posts.find((post) => post.id === id) || {};
+  response.send(post);
+});
 
 app.listen(port, () => {
   console.log(`Example app on port ${port}`);
