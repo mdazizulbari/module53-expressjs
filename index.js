0f4c5ab1980a127d2422c13d2f51384836c9bd96
smabari <mdazizulbarishafin@gmail.com>
2025-05-20 23:12:15 +0600

    showed in the client server and recieved from there too

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-const posts = require("./posts.json");
+// const posts = require("./posts.json");
+const posts = [
+  {
+    userId: 1,
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "qui est esse",
+    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "eum et est occaecati",
+    body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "nesciunt quas odio",
+    body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+  },
+];
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("My Express server");
@@ -12,6 +47,15 @@ app.get("/", (request, response) => {
 
 app.get("/posts", (request, response) => {
   response.send(posts);
+});
+
+app.post("/posts", (request, response) => {
+  console.log("Received body:", request.body);
+  const newPost = request.body;
+  newPost.id = posts.length + 1;
+  // add data to the database
+  posts.push(newPost);
+  response.send(newPost);
 });
 
 app.get("/posts/:id", (request, response) => {
